c9ae645f653db8679f7995534ec71a812ee02da8
Vincent Nys <vincentnys@gmail.com>
2020-03-22 23:51:36 +0100

    Fix.

--- test_python_zelf_functies_schrijven.py
+++ test_python_zelf_functies_schrijven.py
@@ -6,4 +6,4 @@ def _model_solution(number):
 
 @pytest.mark.parametrize("input1",[0,255,128,-1,256])
 def test_is_byte(input1):
-    assert python_printen.is_byte(input1) == _model_solution(input1),f"verschil met modeloplossing voor invoer {input1}"
+    assert python_zelf_functies_schrijven.is_byte(input1) == _model_solution(input1),f"verschil met modeloplossing voor invoer {input1}"
