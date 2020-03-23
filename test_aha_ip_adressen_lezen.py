267ed29d66456a6325c2ee47746d82e01ea1d5db
Vincent Nys <vincentnys@gmail.com>
2020-03-23 10:08:56 +0100

    fix test

--- test_aha_ip_adressen_lezen.py
+++ test_aha_ip_adressen_lezen.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import patch, MagicMock
 import aha_ip_adressen_lezen
 
-def _two_opened_file_mocks(self,model,student):
+def _two_opened_file_mocks(model,student):
     base_mock = MagicMock(name='mock for open')
     mock_context_manager_1 = MagicMock(name='mock for context manager 1')
     mock_context_manager_1.__enter__.return_value.read.return_value = model
@@ -21,6 +21,6 @@ def test_read_ips():
 200.200.200.200
 17.13.8.101
 1.20.150.255"""
-    base_mock = self._two_opened_file_mocks(lines,lines)
+    base_mock = _two_opened_file_mocks(lines,lines)
     with patch('builtins.open',base_mock):
         assert aha_ip_adressen_lezen.read_ips() == _model_solution(), f"Je script levert een foutief resultaat voor minstens één adres in een file met volgende IP's:\n{lines}.\nProbeer zelf de regel vast te stellen waar het fout loopt door de inhoud van je file aan te passen en je script uit te voeren."
