463458b2e059930f2a19b79b671df773f4b7883e
Vincent Nys <vincentnys@gmail.com>
2020-03-27 09:52:12 +0100

    tests

---
+++ test_python_spam_regel_functie.py
@@ -0,0 +1,24 @@
+import pytest
+import importlib
+import sys
+import io
+import random
+import string
+import python_spam_regel_functie
+
+def _random_text_with_word(word):
+    rndm = "".join([random.choice(string.ascii_letters) for _ in range(random.randint(1,10))])
+    return f"{rndm} {word} {rndm}"
+
+@pytest.mark.parametrize("line,outcome",[("buy cheap viagra online",True),\
+                                         ("you have won the lottery",True),\
+                                         ("your great-uncle has left you an inheritance",True),\
+                                         ("belangrijke mededeling",False),\
+                                         ("blabla",False),\
+                                         (_random_text_with_word("viagra"),True),\
+                                         (_random_text_with_word("lottery"),True),\
+                                         (_random_text_with_word("inheritance"),True),\
+                                         ("testtest",False)])
+def test_run_as_script(capfd,monkeypatch,line,outcome):
+    result = python_spam_regel_functie.check_spam_line(line)
+    assert result == outcome, f"""de tekst "{line}" is{" geen" if (not outcome) else ""} spam, maar is in jouw functie anders geklasseerd"""
