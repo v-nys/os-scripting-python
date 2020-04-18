7d62baa9c287bf6c202479c744f60af38034812d
Vincent Nys <vincentnys@gmail.com>
2020-04-18 18:56:48 +0200

    Meer tips.

--- test_python_bitwise_ops.py
+++ test_python_bitwise_ops.py
@@ -10,5 +10,5 @@ def test_read_stdout(capfd,monkeypatch):
     host_address_2_binary_string = "".join([f"{host_byte:08b}" for host_byte in host_address_2])
     import python_bitwise_ops
     captured = capfd.readouterr().out.split("\n")
-    assert captured[0].strip() == host_address_1_network_address_string, "Het gevraagde netwerkadres klopt niet. Je moet een bitwise and van het netmasker en het hostadres doen."
-    assert captured[1].strip() == host_address_2_binary_string, "Het gevraagde hostadres klopt niet. Je moet een bitwise or van het netwerkadres en het hostgedeelte doen."
+    assert captured[0].strip() == host_address_1_network_address_string, "Het gevraagde netwerkadres klopt niet. Je moet een bitwise and van het netmasker en het hostadres doen. Doe dit stap voor stap op papier en kijk na wat jouw code anders doet."
+    assert captured[1].strip() == host_address_2_binary_string, "Het gevraagde hostadres klopt niet. Je moet een bitwise or van het netwerkadres en het hostgedeelte doen. Doe dit stap voor stap op papier en kijk na wat jouw code anders doet."
