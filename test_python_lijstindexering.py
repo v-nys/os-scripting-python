95e5fbc328a131569f68d2a35d96b0cd72c0b5ee
Vincent Nys <vincentnys@gmail.com>
2020-04-20 18:32:53 +0200

    Update na vraag Steve.

--- test_python_lijstindexering.py
+++ test_python_lijstindexering.py
@@ -3,4 +3,4 @@ import pytest
 def test_read_stdout(capfd,monkeypatch):
     import python_lijstindexering
     captured = capfd.readouterr().out.split("\n")
-    assert captured[0].strip() == "hello world how are you?"
+    assert captured[0].strip() == "hello world how are you?", "Je output matcht niet exact met de verwachte output. Dit kan aan een detail liggen. Staat er misschien een extra spatie of worden er aanhalingstekens geprint?"
