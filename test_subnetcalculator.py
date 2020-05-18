1d09b14190df5a63bb6b87c180d0a07297b9b4f9
Vincent Nys <vincentnys@gmail.com>
2020-05-18 14:20:03 +0200

    Hernoemd.

--- test_subnetcalculator.py
+++ test_subnetcalculator.py
@@ -1,5 +1,5 @@
 import pytest
-import python_project_subnet_calculator
+import subnetcalculator
 
 def _is_valid_ip_address(numberlist):
     if len(numberlist) != 4:
@@ -51,19 +51,19 @@ def _prefix_length_to_max_host(netmask_length):
 
 @pytest.mark.parametrize("numberlist",[[192,168,0,191],[1,1,1,1],[192,168,0,191,1],[255,255,255,255],[0,0,0,0],[0,0,-1,0],[254,254,254,254]])
 def test_is_valid_ip_address(numberlist):
-    student = python_project_subnet_calculator.is_valid_ip_address(numberlist)
+    student = subnetcalculator.is_valid_ip_address(numberlist)
     model = _is_valid_ip_address(numberlist)
     assert student == model,f"verschil met modeloplossing is_valid_ip_address voor invoer {input1}: jouw oplossing levert {student}, de modeloplossing levert {model}"
 
 @pytest.mark.parametrize("numberlist",[[192,168,0,191],[1,1,1,1],[192,168,0,191,1],[255,255,255,255],[0,0,0,0],[254,254,254,254],[255,0,0,0],[192,0,0,0],[192,168,0,0],[255,192,0,0]])
 def test_is_valid_netmask(numberlist):
-    student = python_project_subnet_calculator.is_valid_netmask(numberlist)
+    student = subnetcalculator.is_valid_netmask(numberlist)
     model = _is_valid_netmask(numberlist)
     assert student == model,f"verschil met modeloplossing is_valid_netmask voor invoer {input1}: jouw oplossing levert {student}, de modeloplossing levert {model}"
 
 @pytest.mark.parametrize("numberlist",[[255,255,255,255],[0,0,0,0],[255,0,0,0],[192,0,0,0],[255,192,0,0]])
 def test_one_bits_in_netmask(numberlist):
-    student = python_project_subnet_calculator.one_bits_in_netmask(numberlist)
+    student = subnetcalculator.one_bits_in_netmask(numberlist)
     model = _one_bits_in_netmask(numberlist)
     assert student == model,f"verschil met modeloplossing one_bits_in_netmask voor invoer {input1}: jouw oplossing levert {student}, de modeloplossing levert {model}"
 
@@ -73,13 +73,13 @@ def test_one_bits_in_netmask(numberlist):
                                                  ([192,168,0,191],[192,0,0,0]),\
                                                  ([192,168,0,191],[255,192,0,0])])
 def test_apply_network_mask(host_address,netmask):
-    student = python_project_subnet_calculator.apply_network_mask(host_address,netmask)
+    student = subnetcalculator.apply_network_mask(host_address,netmask)
     model = _apply_network_mask(host_address,netmask)
     assert student == model,f"verschil met modeloplossing apply_network_mask voor hostadres {host_address} en netmasker {netmask}: jouw oplossing levert {student}, de modeloplossing levert {model}"
 
 @pytest.mark.parametrize("numberlist",[[255,255,255,255],[0,0,0,0],[255,0,0,0],[192,0,0,0],[255,192,0,0]])
 def test_netmask_to_wildcard_mask(numberlist):
-    student = python_project_subnet_calculator.netmask_to_wildcard_mask(numberlist)
+    student = subnetcalculator.netmask_to_wildcard_mask(numberlist)
     model = _netmask_to_wildcard_mask(numberlist)
     assert student == model,f"verschil met modeloplossing netmask_to_wildcard_mask voor invoer {input1}: jouw oplossing levert {student}, de modeloplossing levert {model}"
 
@@ -89,12 +89,12 @@ def test_netmask_to_wildcard_mask(numberlist):
                                                        ([192,168,0,191],[63,255,255,255]),\
                                                        ([192,168,0,191],[0,63,255,255])])
 def test_apply_network_mask(host_address,wildcard_mask):
-    student = python_project_subnet_calculator.get_broadcast_address(host_address,wildcard_mask)
+    student = subnetcalculator.get_broadcast_address(host_address,wildcard_mask)
     model = _get_broadcast_address(host_address,wildcard_mask)
     assert student == model,f"verschil met modeloplossing get_broadcast_address voor hostadres {host_address} en wildcard mask {wildcard_mask}: jouw oplossing levert {student}, de modeloplossing levert {model}"
 
 
 def test_prefix_length_to_max_hosts(prefix_length):
-    student = python_project_subnet_calculator.prefix_length_to_max_hosts(prefix_length)
+    student = subnetcalculator.prefix_length_to_max_hosts(prefix_length)
     model = _prefix_length_to_max_host(prefix_length)
     assert student == model,f"verschil met modeloplossing prefix_length_to_max_hosts voor lengte {prefix_length}: jouw oplossing levert {student}, de modeloplossing levert {model}"
